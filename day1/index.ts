9b1e57a3f9d53bc42ca35aca9604439bf824016a
Rita Leitao (CTW) <rita.leitao@ctw.bmwgroup.com>
2024-12-03 20:04:21 +0000

    Day 1

---
+++ day1/index.ts
@@ -0,0 +1,29 @@
+import { input } from "./input";
+import { calculateDistance, getLines } from "../utils";
+
+// PT1
+
+const lines = getLines(input);
+
+const sortedLeftColumn = lines.map(([a, _]) => a).sort((a, b) => a - b);
+const sortedRightColumn = lines.map(([_, b]) => b).sort((a, b) => a - b);
+
+const totalDistance = sortedLeftColumn.reduce(
+	(accumulator, leftValue, index) => accumulator + calculateDistance(leftValue, sortedRightColumn[index]),
+	0
+);
+
+console.log(totalDistance);
+
+// PT2
+
+const getSimilarityScore = (leftColumn: number[], rightColumn: number[]) => {
+	return leftColumn
+		.map((el) => {
+			const occurences = rightColumn.filter((item) => item === el).length;
+			return occurences * el;
+		})
+		.reduce((acc, el) => acc + el, 0);;
+};
+
+console.log(getSimilarityScore(sortedLeftColumn, sortedRightColumn));
